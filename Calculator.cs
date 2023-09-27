a09c18e018581754c54235b04ae8c1c28a180102
maks11455 <maks101330@mail.ru>
2023-09-27 21:26:30 +0000

    first Commit

---
+++ Calculator.cs
@@ -0,0 +1,35 @@
+﻿
+double firstValue, secondValue;
+string action;
+
+ Console.WriteLine("Введи число 1");
+ firstValue = double.Parse(Console.ReadLine());
+
+ Console.WriteLine("Введи число 2");
+ secondValue = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Веберите операцию: '+' '-' '*' '/' " );
+action = Console.ReadLine();
+switch (action)
+{
+
+    case "+":
+      Console.WriteLine(firstValue + secondValue);
+       break;
+     case "-":  
+      Console.WriteLine(firstValue - secondValue);
+      break;
+     case "*":  
+      Console.WriteLine(firstValue * secondValue);
+     break;
+     case "/": 
+     if (secondValue == 0)
+     Console.WriteLine(0); 
+      Console.WriteLine(firstValue / secondValue);
+      break;
+    default:
+    Console.WriteLine("Ошибка!");
+      break;
+}
+
+
